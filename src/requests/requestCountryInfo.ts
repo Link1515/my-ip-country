0d59af1a705b43835e1bbb2a321afea59947ddc2
Link1515 <h26281660@gmail.com>
2024-11-07 11:36:56 +0800

    create request: requestCountryInfo

---
+++ src/requests/requestCountryInfo.ts
@@ -0,0 +1,47 @@
+const API_URL =
+  "https://restcountries.com/v3.1/alpha/<country>?fields=name,currencies,capital,capitalInfo,region,subregion,languages,maps,population,flag,flags";
+
+interface Response {
+  name: Name;
+  capital: string[];
+  capitalInfo: CapitalInfo;
+  region: string;
+  subregion: string;
+  currencies: { [abbreviation: string]: Currencies };
+  flag: string;
+  flags: Flags;
+  languages: { [abbreviation: string]: string };
+  maps: Maps;
+  population: number;
+}
+
+interface Name {
+  common: string;
+  official: string;
+}
+
+interface CapitalInfo {
+  latlng: [latitude: number, longitude: number];
+}
+
+interface Currencies {
+  name: string;
+  symbol: string;
+}
+
+interface Flags {
+  png: string;
+  svg: string;
+  alt: string;
+}
+
+interface Maps {
+  googleMaps: string;
+  openStreetMaps: string;
+}
+
+export const requestCountryInfo = async (country: string) => {
+  return await fetch(API_URL.replace("<country>", country)).then<Response>(
+    (res) => res.json(),
+  );
+};

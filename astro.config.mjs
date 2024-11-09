07b6c571728a2cdd32338dae2090f00b4e4c4b60
Link1515 <h26281660@gmail.com>
2024-11-09 11:26:34 +0800

    set github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://link1515.github.io",
+  base: "my-ip-country",
   integrations: [
     tailwind({
-      applyBaseStyles: false
-    })
-  ]
+      applyBaseStyles: false,
+    }),
+  ],
 });
